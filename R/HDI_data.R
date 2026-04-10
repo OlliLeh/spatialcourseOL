19c04deedc9f05d4cef17e640ea00a7ce48d7139
OlliLeh <olli.lehtonen@uef.fi>
2026-04-10 15:00:58 +0300

    Update README and rebuild site

---
+++ R/HDI_data.R
@@ -0,0 +1,22 @@
+#' Human Development Index (HDI) data
+#'
+#' A dataset containing Human Development Index (HDI) indicators
+#' published by the United Nations Development Programme (UNDP).
+#' The data include country-level HDI values and related indicators
+#' for multiple years.
+#'
+#' @format A data frame with one row per country-year and the following variables:
+#' \describe{
+#'   \item{Country}{Country name}
+#'   \item{HDI}{Human Development Index value}
+#' }
+#'
+#' @source
+#' United Nations Development Programme (UNDP).
+#' \emph{Human Development Report 2025, Statistical Annex}.
+#' \url{https://hdr.undp.org/}
+#'
+#' @usage data(HDI_data)
+#'
+#' @keywords datasets
+"HDI_data"
