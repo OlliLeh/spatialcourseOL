852915b765d44529cc079ad4307b7f000651bd0c
OlliLeh <olli.lehtonen@uef.fi>
2026-04-12 21:49:03 +0300

    Update lecture content

---
+++ R/pop_category.R
@@ -0,0 +1,19 @@
+#' Population development by municipality category
+#'
+#' Components of population change by urban-rural municipality category.
+#'
+#' @format A data frame in long format with 360 rows and 4 variables:
+#' \describe{
+#'   \item{year}{Year}
+#'   \item{category}{Urban–rural municipality category}
+#'   \item{value}{Population change in persons}
+#'   \item{variable}{Demographic component of population change}
+#' }
+#'
+#' @source
+#' Statistics Finland.
+#' \emph{Migration and population change database}.
+#' \url{https://stat.fi/fi/palvelut/tilastodatapalvelut/tilastotietokannat/vaestotilastopalvelu/muuttoliike-ja-vaestonmuutokset}
+#'
+#' @keywords datasets
+"pop_category"
